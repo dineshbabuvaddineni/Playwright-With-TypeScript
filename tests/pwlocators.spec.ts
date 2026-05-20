2ff637ab903747c89df9b0fc2d7d75696d6c99a3
dineshbabuvaddineni <dineshbabuvaddineni@gmail.com>
2026-05-20 23:15:43 +0530

    locators2

--- tests/pwlocators.spec.ts
+++ tests/pwlocators.spec.ts
@@ -1,6 +1,7 @@
 /*
+In JS we don't sepcify where in ts we should follow the type inference concept
 
-Locators - used to find elements on the web
+Locators - used to indentify the  elements on the web
 Locators works on based on the DOM structure
 DOM -Document Object Model
 
@@ -17,3 +18,19 @@ page.getByTitle() to locate an element by its title attribute.
 page.getByTestId() to locate an element based on its data-testid attribute (other attributes can be configured).
 
 */
+
+//https://demo.nopcommerce.com/     ---> url used for verifying the locators
+
+import {test,expect,Locator} from "@playwright/test"
+test("Verify Playwright Locators",async({page})=>{
+    await page.goto("https://demo.nopcommerce.com/");
+
+    // 1. page.getByAltText() - identifies images (and similar elements) based on the alt attributes
+    // use this locator when your element supports alt text as img and area elements.
+     const logo:Locator= page.getByAltText("nopCommerce demo store");
+     await expect(logo).toBeVisible();
+
+
+
+
+})

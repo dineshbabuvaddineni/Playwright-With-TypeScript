3b1d97f94e52b84903dd73fdf45901ed58a2c916
dineshbabuvaddineni <dineshbabuvaddineni@gmail.com>
2026-06-25 23:53:29 +0530

    assertion

--- tests/autoWaitingandtimeout.spec.ts
+++ tests/autoWaitingandtimeout.spec.ts
@@ -1,11 +1,17 @@
 import {test, expect} from '@playwright/test';
 
 test('Autowaiting and forcing',async({page})=>{
+
+    test.setTimeout(50000); //50secs
+    // test.slow(); //90 secs (default is 30 secs) -->It makes triple the timeout
+
     await page.goto('https://demowebshop.tricentis.com/');
 
      //Assertions - Auto wait works
-    await expect(page).toHaveURL("https://demowebshop.tricentis.com/");
-    await expect(page.locator('text=Welcome to our store')).toBeVisible();
+    await expect(page).toHaveURL("https://demowebshop.tricentis.com/",{timeout:10000});
+    await expect(page.locator('text=Welcome to our store')).toBeVisible({timeout:10000});
+
+    //for assertions force action not works 
 
     //Actions- Auto wait works
     await page.locator('#small-searchterms').fill("Laptop",{force:true}); //search box- Force Action(It will not do actionability checks)
